5131b8cf2364253c2cd12e594dc7c679df63fd08
Vikas <vikasgowda189@gmai.com>
2024-01-10 17:50:17 +0530

    added actions to chat app

--- app/assets/javascript/room.js
+++ app/assets/javascript/room.js
@@ -1,5 +1,25 @@
 $(function() {
-    $('#new_room_message').on('ajax:success', function(a, b,c ) {
-      $(this).find('input[type="text"]').val('');
+  $('#new_room_message').on('ajax:success', function(event, data, status, xhr) {
+    // Extract the CSRF token from meta tags
+    var csrfToken = $('meta[name="csrf-token"]').attr('content');
+
+    // Include the CSRF token in the headers of the next AJAX request
+    $.ajax({
+      type: 'POST',
+      url: '/your_ajax_endpoint',
+      headers: {
+        'X-CSRF-Token': csrfToken
+      },
+      data: {
+        // Your data here
+      },
+      success: function(response) {
+        // Handle success
+        $(this).find('input[type="text"]').val('');
+      },
+      error: function(xhr, status, error) {
+        // Handle error
+      }
     });
   });
+});
--- app/controllers/room_messages_controller.rb
+++ app/controllers/room_messages_controller.rb
@@ -1,15 +1,16 @@
 class RoomMessagesController < ApplicationController
-    before_action :load_entities
+  protect_from_forgery with: :exception
+  before_action :load_entities
   
-    def create
-      @room_message = RoomMessage.create user: current_user,
-                                         room: @room,
-                                         message: params.dig(:room_message, :message)
-    end
-  
-    protected
-  
-    def load_entities
-      @room = Room.find params.dig(:room_message, :room_id)
-    end
+  def create
+    @room_message = RoomMessage.create user: current_user,
+                                        room: @room,
+                                        message: params.dig(:room_message, :message)
   end
+  
+  protected
+  
+  def load_entities
+    @room = Room.find params.dig(:room_message, :room_id)
+  end
+end
